1c388174c21eabd935cb9517cbd54152c05b9e87
Your Name <you@example.com>
2021-06-12 02:14:43 +0500

    this is complete working code of first two sections

--- EAD_Project/EAD_Project/Client/obj/Debug/netstandard2.1/RazorDeclaration/Shared/MainLayout.razor.g.cs
+++ EAD_Project/EAD_Project/Client/obj/Debug/netstandard2.1/RazorDeclaration/Shared/MainLayout.razor.g.cs
@@ -13,70 +13,70 @@ namespace EAD_Project.Client.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\UMAIR AHMED\source\repos\EAD_Project\EAD_Project\Client\_Imports.razor"
+#line 1 "C:\EAD\EAD_Project\EAD_Project\Client\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\UMAIR AHMED\source\repos\EAD_Project\EAD_Project\Client\_Imports.razor"
+#line 2 "C:\EAD\EAD_Project\EAD_Project\Client\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\UMAIR AHMED\source\repos\EAD_Project\EAD_Project\Client\_Imports.razor"
+#line 3 "C:\EAD\EAD_Project\EAD_Project\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\UMAIR AHMED\source\repos\EAD_Project\EAD_Project\Client\_Imports.razor"
+#line 4 "C:\EAD\EAD_Project\EAD_Project\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\UMAIR AHMED\source\repos\EAD_Project\EAD_Project\Client\_Imports.razor"
+#line 5 "C:\EAD\EAD_Project\EAD_Project\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\UMAIR AHMED\source\repos\EAD_Project\EAD_Project\Client\_Imports.razor"
+#line 6 "C:\EAD\EAD_Project\EAD_Project\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\UMAIR AHMED\source\repos\EAD_Project\EAD_Project\Client\_Imports.razor"
+#line 7 "C:\EAD\EAD_Project\EAD_Project\Client\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\UMAIR AHMED\source\repos\EAD_Project\EAD_Project\Client\_Imports.razor"
+#line 8 "C:\EAD\EAD_Project\EAD_Project\Client\_Imports.razor"
 using EAD_Project.Client;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\UMAIR AHMED\source\repos\EAD_Project\EAD_Project\Client\_Imports.razor"
+#line 9 "C:\EAD\EAD_Project\EAD_Project\Client\_Imports.razor"
 using EAD_Project.Client.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\UMAIR AHMED\source\repos\EAD_Project\EAD_Project\Client\_Imports.razor"
+#line 10 "C:\EAD\EAD_Project\EAD_Project\Client\_Imports.razor"
 using EAD_Project.Shared;
 
 #line default
